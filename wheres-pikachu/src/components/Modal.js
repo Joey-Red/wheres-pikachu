735e141404a1758cf279d579db11b1c09e428a17
Joey Dalrymple <xjoey96@gmail.com>
2022-03-23 03:05:27 -0600

    toyed with function, figured out the way forward

--- wheres-pikachu/src/components/Modal.js
+++ wheres-pikachu/src/components/Modal.js
@@ -2,46 +2,47 @@ import React from 'react'
 import PokemonPic from '../img/PokemonPic.png'
 import '../style.css'
 function Modal() {
-  // let el = document.getElementById("wwPic")
-  // function showCoords(el) {
-  // const rect = el.getBoundingClientRect();
-  // console.log("left: " + rect.left + window.scrollX + "top: " + rect.top + window.scrollY)
-  // return {
-  //   left: rect.left + window.scrollX,
-  //   top: rect.top + window.scrollY
-  // };
+  // let clicked = (e) => {
+  //   let imgX = e.pageX;
+  //   let imgY = e.pageY;
+  //   console.log(imgX, imgY);
   // }
-  let clicked = (e) => {
-    // let imgX = e.pageX;
-    // let imgY = e.pageY;
-    // console.log(imgX, imgY);
-  }
+  // function getPosition(event) {
+  //     console.log("X: " + event.clientX + "Y: " + event.clientY);
+  //   }
+  // document.getElementById('wwPic').onclick = function clickEvent(e) {
+  //     // e = Mouse click event.
+  //     var rect = e.target.getBoundingClientRect();
+  //     var x = e.clientX - rect.left; //x position within the element.
+  //     var y = e.clientY - rect.top;  //y position within the element.
+  //     console.log("Left? : " + x + " ; Top? : " + y + ".");
+  //   }
 
-  function getPosition(el) {
-  var xPosition = 0;
-  var yPosition = 0;
-  while (el) {
-    if (el.tagName === "BODY") {
-      var xScrollPos = el.scrollLeft || document.documentElement.scrollLeft;
-      var yScrollPos = el.scrollTop || document.documentElement.scrollTop;
-      xPosition += (el.offsetLeft - xScrollPos + el.clientLeft);
-      yPosition += (el.offsetTop - yScrollPos + el.clientTop);
-    } else {
-      xPosition += (el.offsetLeft - el.scrollLeft + el.clientLeft);
-      yPosition += (el.offsetTop - el.scrollTop + el.clientTop);
-    }
-    el = el.offsetParent;
-  }
-  return {
-    x: xPosition,
-    y: yPosition
-    };
-  }
+  // function getCords(e){
+  // const x = e.offsetX;
+  // const y = e.offsetY;
+  // console.log(x, y)
+  // }
+  let picVar = document.getElementById('wwPic');
+  // function getCords(e){
+  //   let x = e.pageX - this.offsetLeft - picVar.offset().left;
+  //   let y = e.pageY - this.offsetTop - picVar.offset().top;
+  //   console.log(x,y)
+  // }
+  document.body.onclick = function(e) {
+    e = e || window.event;
+    var target = e.target || e.picVar,
+        rect = target.getBoundingClientRect(),
+        offsetX = e.clientX - rect.left,
+        offsetY = e.clientY - rect.top;
+
+    console.log([offsetX, offsetY]);
+};
+
   return (
-    
     <div className='modal-container'>
       <div className="modal">
-        <img src={PokemonPic} onClick={getPosition()} id="wwPic" alt="Pokemon" />
+        <img src={PokemonPic} id="wwPic" alt="Pokemon" />
       </div>
     </div>
   )
