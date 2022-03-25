a53a57bd6bc7a5885d11850285acd6666065a760
Joey Dalrymple <xjoey96@gmail.com>
2022-03-25 15:41:16 -0600

    minor changes

--- wheres-pikachu/src/components/Modal.js
+++ wheres-pikachu/src/components/Modal.js
@@ -4,23 +4,29 @@ import '../style.css';
 function Modal() {
   let [cords, setCords] = useState([0,0]);
 
+  // let handleMouseClick = (e) => {
+  //   setCords([e.clientX, e.clientY]);
+  //   console.log(cords)
+  //   let scanBox = document.querySelector('.scanBox')
+  //   scanBox.style.display="flex";
+  //   // Maybe in the future set a timer so that it dissapears shortly
+  // }
+  
   let handleMouseClick = (e) => {
-    setCords([e.clientX, e.clientY]);
+    setCords([e.clientX += e.offsetTop  || 0, e.clientY += e.offsetLeft || 0], console.log(cords));
     let scanBox = document.querySelector('.scanBox')
     scanBox.style.display="flex";
-    // Maybe in the future set a timer so that it dissapears shortly
   }
   return (
   <>
       <div className='scanBox'
       style={{
-        backgroundColor: 'orange',
-        height: 17,
-        width: 17,
-        padding: '15px',
+        height: 20,
+        width: 20,
+        padding: '20px',
         position: 'absolute',
-        left: cords[0],
-        top: cords[1]
+        left: cords[0] - 40,
+        top: cords[1] - 40
       }}></div>
     <div className='modal-container'>
       <div className="modal" onClick={handleMouseClick}>
