f3ea7193b1e214fef7184f1f601a1abee54d025d
Joey Dalrymple <xjoey96@gmail.com>
2022-03-25 02:58:35 -0600

    got function to work properly.

--- wheres-pikachu/src/components/App.js
+++ wheres-pikachu/src/components/App.js
@@ -2,18 +2,15 @@ import Score from "./Score";
 import Objectives from "./Objectives";
 import Header from "./Header";
 import Modal from "./Modal";
-// import ClickHandlerv3 from "./ClickHandlerv3";
 function App() {
-  
   return (
     <div className="App">
-      {/* <ClickHandlerv3/> */}
       <div className="header-bar">
         <Score />
         <Header />
       </div>
       <div className="modal-obj">
-        <Modal />
+        <Modal/>
         <Objectives />
       </div>
     </div>
--- wheres-pikachu/src/components/Modal.js
+++ wheres-pikachu/src/components/Modal.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PokemonPic from '../img/PokemonPic.png'
 import '../style.css';
 function Modal() {
+  let [cords, setCords] = useState([0,0]);
+
+  let handleMouseClick = (e) => {
+    setCords([e.clientX, e.clientY]);
+    let scanBox = document.querySelector('.scanBox')
+    scanBox.style.display="flex";
+    // Maybe in the future set a timer so that it dissapears shortly
+  }
   return (
+  <>
+      <div className='scanBox'
+      style={{
+        backgroundColor: 'orange',
+        height: 17,
+        width: 17,
+        padding: '15px',
+        position: 'absolute',
+        left: cords[0],
+        top: cords[1]
+      }}></div>
     <div className='modal-container'>
-      <div className="modal">
-        <img src={PokemonPic} id="wwPic" alt="Pokemon" />
+      <div className="modal" onClick={handleMouseClick}>
+        <img src={PokemonPic} id="wwPic" alt="Pokemon"/>
       </div>
     </div>
+  </>
   )
 }
 
